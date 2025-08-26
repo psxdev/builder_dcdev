4f6a250b84b0c3e15958ec0a89979290e2374617
Antonio Jose Ramos Marquez <psxdev@gmail.com>
2025-08-26 11:19:40 +0200

    fix builder

--- build.sh
+++ build.sh
@@ -4,18 +4,14 @@ cd /usr/local/dcdev
 git clone https://github.com/psxdev/KallistiOS -b macport kos
 cp ${GITHUB_WORKSPACE}/config/Makefile.cfg kos/utils/dc-chain
 cd kos/utils/dc-chain
-export CPATH=/opt/local/include
-export LIBRARY_PATH=/opt/local/lib
 make
 make clean distclean
 cd ../..
-unset CPATH
-unset LIBRARY_PATH
 cp ${GITHUB_WORKSPACE}/config/environ.sh .
 source /usr/local/dcdev/kos/environ.sh
 make
 cd ..
-git clone https://github.com/psxdev/kos-ports -b macport
+git clone https://github.com/psxdev/kos-ports
 /usr/local/dcdev/kos-ports/utils/build-all.sh
 
  
